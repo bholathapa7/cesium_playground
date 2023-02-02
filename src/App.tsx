0d9c74f102f0ed8077d26613bcd21c8ed8df4d5f
Reuben Thapa <reubenthapa@Reubens-MacBook-Pro.local>
2023-02-02 13:42:57 +0545

    feat: building and powerplant BIM added

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,38 @@ import { useEffect } from 'react';
 
 function App() {
   useEffect(() => {
-    const viewer = new Cesium.Viewer('cesiumContainer', {
-      terrainProvider: Cesium.createWorldTerrain(),
+    const viewer = new Cesium.Viewer('cesiumContainer');
+    const scene = viewer.scene;
+
+    viewer.clock.currentTime = Cesium.JulianDate.fromIso8601(
+      '2023-02-02T00:00:00Z'
+    );
+
+    const tileset = viewer.scene.primitives.add(
+      new Cesium.Cesium3DTileset({
+        url: 'src/assets/tileset.json', // for buildings
+        // url: 'src/assets/tileset_demo.json',  // for powerplant
+      })
+    );
+    tileset.readyPromise.then(function (tileset: any) {
+      console.log({ tileset });
+      if (tileset) {
+        viewer
+          .zoomTo(
+            tileset,
+            new Cesium.HeadingPitchRange(
+              0.5,
+              -0.2,
+              tileset.boundingSphere.radius * 4.0
+            )
+          )
+          .catch(function (error: any) {
+            console.log(error);
+          });
+      }
     });
+    tileset.colorBlendMode = Cesium.Cesium3DTileColorBlendMode.REPLACE;
+
     return () => {
       viewer.destroy();
     };
